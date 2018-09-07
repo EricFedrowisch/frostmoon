8c3ee1a6b89d474e92e98383df66c9233814ec01
Eric Fedrowisch <efedrowisch@yahoo.com>
2018-09-07 15:17:50 -0500

    Basic Loader Done
    -Loader is done for now, may optimize/clean up code later but for now its working
    -Went with flat table rather than nested tables for hierarchal purposes of the recursive loading
    -Table of components uses dot notation to denote hierarchy. Ie "gui.button"
    -Commented on some of the more opaque lines
    -NOTE: Only folders in the folder set by the "componet_dir" variable will be added as components. The target dir should also be in the same directory as frostmoon.lua file itself

--- frostmoon.lua
+++ frostmoon.lua
@@ -1,14 +1,14 @@
 --[[
-#TODO: Give local environment?
-#TODO: Add a before/after comparison of the global Environment to raise warnings
-if components pollute global namespace accidently
-#TODO: Add CPath searcher stuff too
+#TODO:Add warnings for:component already exists, global namespace polluted by loaded
+      component
+#TODO: Add CPath searcher & loading capabilities
 ]]
-
+local component_dir = "components" --Directory from which to recursively load components
 local lfs = require("lfs")
 local os_sep = package.config:sub(1,1) --Get the OS file path seperator
 local d = require("frost_debug")
 local exports = {} --Temp storage for exported functionality
+
 --"Table of Contents" for exports of the module
 local function export()
    return {
@@ -26,26 +26,16 @@ local function split(str, sep)
    return t
 end
 
-local function component_lookup(str_path)
-   local t, load_str={}, 'return components'
-   for str in string.gmatch(str_path, "([^"..'.'.."]+)") do t[#t+1] = str end
-   for i,str in ipairs(t) do str = str:gsub('%W','') end --Scrub non-alphanumeric
-   for i,v in pairs(t) do load_str = load_str .. '["' .. v ..  '"]' end
-   local results, errors = loadstring(load_str)
-   if errors then print("Error:", errors, "load_str:", load_str) end
-   return results()
-end
-
 local function load_components(dir, recurse, components)
    local components = components or {}
    local files, dirs = {}, {}
-   package.path = package.path .. ";" .. dir .. os_sep .. "?.lua"
+   package.path = package.path .. ";" .. dir .. os_sep .. "?.lua" --Add to path for require
    for i in lfs.dir(dir) do
-      if (i ~= ".") and (i ~= "..") then --#TODO:This work on Windows & Linux?
+      if (i ~= ".") and (i ~= "..") then --#TODO:Check does this work on Windows, iOS & Linux?
          local f = dir .. os_sep .. i
          if lfs.attributes(f,"mode") == "file" then
             local parse = {f:match("(.-)([^\\/]-([^\\/%.]+))$")} --String MAAAAAAGIC!
-            if (parse[3]:lower() == 'lua') and (parse[2] ~= arg[0]) then
+            if (parse[3]:lower() == 'lua') and (parse[2] ~= arg[0]) then --If lua and not this script itself
                files[#files+1]=f
             end
          end
@@ -53,19 +43,29 @@ local function load_components(dir, recurse, components)
       end
    end
    for i,file in ipairs(files) do
-      local f = split(file,os_sep) --#TODO: UGLY. Ineffcient. Replace!
-      local up = tostring(f[#f - 1])
-      local f = split(f[#f],".")
-      local f = f[1]
-      --if not components[up] then components[up] = {} end
-      --components[up][f]=require(f)
-      components[f]=require(f)
+      local f = split(file,os_sep)
+      local split_index = nil
+      local name = split(f[#f],".");name = name[1];f[#f]=name --Get file name minus '.lua'
+      for i,v in ipairs(f) do  --Find componet dir index
+         if f[i] == component_dir then split_index = i; break end
+      end
+      for i,v in ipairs(f) do --Delete preceding path before
+         if i < split_index then f[i] = nil end
+      end
+      local t = {}
+      for i = split_index + 1,#f do t[#t+1] = f[i] end
+      local path = table.concat(t,'.');
+      components[path]=require(name)
+      --Do component private variables here
+      components[path]._type = path
+      components[path]._load_path = file
    end
    for i,dir in ipairs(dirs) do
       components = load_components(dir, true, components)
    end
    return components
 end
+
 
 ------------------------------------------
 
@@ -91,11 +91,12 @@ exports.new = new
 --os.execute('clear')
 
 --[[Exercising the code here]]--
+orig_packagepath = package.path --Store package.path before
 local original_cwd = lfs.currentdir()
-local target_dir = lfs.currentdir() .. os_sep .. "components" .. os_sep
+local target_dir = lfs.currentdir() .. os_sep .. component_dir .. os_sep
 lfs.chdir(target_dir)
 exports.components = load_components(lfs.currentdir(), true)
 lfs.chdir(original_cwd)
 d.tprint(exports.components)
---return components
+package.path = orig_packagepath --Restore package.path
 return export()
